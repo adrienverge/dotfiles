fc05e27457d581aba911532438b4e1bb000e9303
Adrien Vergé <adrienverge@gmail.com>
2025-08-20 12:41:03 +0200

    Vim: Fix LSP for mason-lspconfig 2+ and Neovim 0.11+
    This reverts previous commit "Vim: Disable Pyright" that wasn't
    effective, and fixes the `servers = { … }` configuration that was never
    really applied, because of a recent change in Mason.
    
    I debugged it and found that it was caused by this change:
    https://github.com/mason-org/mason-lspconfig.nvim/commit/80f2cd773
    which affects other users:
    https://github.com/nvim-lua/kickstart.nvim/issues/1595
    and has ongoing pull requests to fix. To clean my homemade fix, I
    inspired from this one:
    https://github.com/nvim-lua/kickstart.nvim/pull/1590/files

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -645,12 +645,6 @@ require('lazy').setup({
         },
       }
 
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
@@ -661,6 +655,15 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = 'off',
+              },
+            },
+          },
+        },
         ts_ls = {},  -- Adrien: typescript-language-server
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 
@@ -699,19 +702,13 @@ require('lazy').setup({
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
+      for server, config in pairs(servers) do
+        vim.lsp.config(server, config)
+      end
+
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
+        automatic_enable = true, -- automatically run vim.lsp.enable() for all servers that are installed via Mason
       }
     end,
   },
